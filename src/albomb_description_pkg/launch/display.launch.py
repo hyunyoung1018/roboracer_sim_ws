ca5bb34aa6d0d8f1737504bfb8f8914da572f166
Hyunyoung <imsunghy@gmail.com>
2026-07-30 18:52:25 +0900

    Initial commit

---
+++ src/albomb_description_pkg/launch/display.launch.py
@@ -0,0 +1,39 @@
+import os
+from launch import LaunchDescription
+from launch.substitutions import Command, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+    pkg_share = FindPackageShare('albomb_description_pkg').find('albomb_description_pkg')
+    
+    default_model_path = PathJoinSubstitution([pkg_share, 'urdf', 'albomb.urdf.xacro'])
+    
+    default_rviz_config_path = PathJoinSubstitution([pkg_share, 'rviz', 'albomb.rviz'])
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', default_model_path])}]
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        parameters=[{'robot_description': Command(['xacro ', default_model_path])}]
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', default_rviz_config_path],
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        joint_state_publisher_gui_node,
+        rviz_node
+    ])
